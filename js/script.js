d62b7215a93517007d2aaf99d90e59fdaabcdd0e
Matheus Galindo <matheusgalindo1313@gmail.com>
2022-09-01 23:57:54 -0300

    ajustes layout, segundo bloco com quizzes do usuário, funções de clicar em um quizz e abrir main_2 criada

--- js/script.js
+++ js/script.js
@@ -1,21 +1,49 @@
 const urlAPI = 'https://mock-api.driven.com.br/api/v4/buzzquizz'
 
+let lista_id_ext = []
+let lista_id_int = []
+
+// PEGAR OS QUIZZES DO SERVIDOR E OS DISPOR NA TELA 1
 function getQuizzes() {
     const promise = axios.get (`${urlAPI}/quizzes`)
     promise.then(renderizarQuizzes);
 }
 
 function renderizarQuizzes(resposta) {
+
     console.log(resposta.data);
+
     const quizzes = document.querySelector('.quizzes');
+    
     for (let i = 0; i < 51; i++) {
         quizzes.innerHTML = quizzes.innerHTML + `
-        <div data-identifier="quizz-card" class= "quizz ${resposta.data[i].id}">
+        <div data-identifier="quizz-card" onclick="openQuizz(this)" class= "quizz ${resposta.data[i].id}">
             <img src=${resposta.data[i].image}>
             <p>${resposta.data[i].title}</p>
         </div>
         `
+        lista_id_ext.push(resposta.data[i].id);
     }
 }
 
 getQuizzes()
+console.log(lista_id_ext);
+
+// INTERAÇÃO DE CLICAR EM UM QUIZZ E ABRIR A TELA 2 COM AS PERGUNTAS 
+function openQuizz(selecionado) {
+    const id_selecionado = selecionado.classList[1];
+    const promise = axios.get (`${urlAPI}/quizzes/${id_selecionado}`);
+    promise.then(renderizarQuizzSelecionado);
+    console.log(selecionado.classList);
+}
+
+function renderizarQuizzSelecionado(resposta) {
+    const main_1 = document.querySelector('.main_1');
+    const main_2 = document.querySelector('.main_2');
+    toggleMain(main_1, main_2);
+}
+
+function toggleMain(main_x, main_y) {
+    main_x.classList.add('hidden');
+    main_y.classList.remove('hidden');
+}
