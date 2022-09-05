c7f74f5866404c4582b24d243cf06f552e7ad930
Matheus <matheusvmja@gmail.com>
2022-09-04 23:13:46 -0300

    script tela 2

--- js/script-2.js
+++ js/script-2.js
@@ -1,3 +1,40 @@
-function verificarResposta(resposta) {
-    console.log(resposta.classList);
-}
+function checkIfAnswerSelected(optionsList) {
+    let shouldSelectFlag = true;
+    optionsList.forEach((option) => {
+      if (option.classList.contains("chosen-answer")) {
+        shouldSelectFlag = false;
+      }
+    });
+  
+    return shouldSelectFlag;
+  }
+  
+  function verificarResposta(resposta, questionNumber, totalQuestionQuantity) {
+    const optionsGroup = document.querySelectorAll(`.question${questionNumber}`);
+  
+    optionsGroup.forEach((option) => {
+      if (option.classList.contains("true")) {
+        option.classList.add("right-answer");
+      } else {
+        option.classList.add("wrong-answer");
+      }
+      option.classList.add("non-chosen-answer");
+    });
+  
+    const shouldSelectAnswer = checkIfAnswerSelected(optionsGroup);
+  
+    if (shouldSelectAnswer) {
+      if (resposta.classList.contains("true")) {
+        correctAnswers++;
+      }
+      answeredQuestions++;
+      resposta.classList.add("chosen-answer");
+      if (answeredQuestions === totalQuestionQuantity) {
+        console.log(
+          `respondeu tudo. O seu acerto foi de ${Math.round(
+            (correctAnswers / totalQuestionQuantity) * 100
+          )}%`
+        );
+      }
+    }
+  }
